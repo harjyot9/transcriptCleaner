513380c287fb2cbc91699eb958f961a312d04155
harjyot9 <129429475+harjyot9@users.noreply.github.com>
2024-11-12 00:24:11 -0600

    Add files via upload

---
+++ transcripts.py
@@ -0,0 +1,29 @@
+def numorstr(input):
+    try:
+        int(input[0])
+        return True
+    except:
+        return False
+    
+
+file = "transcript.txt"
+textfile = open(file, "r")
+inputTranscript = textfile.read()
+lines = inputTranscript.splitlines()
+output = open(file, "w") 
+
+for line in lines:
+    words = line.split(" ", 1)
+    first_word = words[0]
+    if numorstr(first_word) == True:
+        pass
+    else:
+        output.write(line + " ")
+
+
+
+
+
+
+
+    
